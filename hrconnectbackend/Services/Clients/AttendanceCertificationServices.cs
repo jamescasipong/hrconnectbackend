c886e8b64928287d08884aca949fe213c641b370
Arisuuuuu <jamesxcasipong@gmail.com>
2025-04-06 00:01:15 +0800

    fix error

--- hrconnectbackend/Services/Clients/AttendanceCertificationServices.cs
+++ hrconnectbackend/Services/Clients/AttendanceCertificationServices.cs
@@ -1,5 +1,6 @@
 ﻿using hrconnectbackend.Data;
 using hrconnectbackend.Interface.Services;
+using hrconnectbackend.Interface.Services.Clients;
 using hrconnectbackend.Models;
 using hrconnectbackend.Repository;
 
